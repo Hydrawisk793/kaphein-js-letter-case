8dc95cdc09cbad06e475c6aeec819a97741e38ae
Hydrawisk793 <hyw793@gmail.com>
2021-04-02 06:05:26 +0100

    refactor: split functions into separate files.

--- src/to-capitalized-case.d.ts
+++ src/to-capitalized-case.d.ts
@@ -1,9 +1,3 @@
-export declare function toDelimiterSeparatedCase(
-    text : string,
-    delimiter? : string,
-    option? : {}
-) : string;
-
 export declare function toCapitalizedCase(
     text : string,
     delimiter? : string | RegExp,
--- src/to-delimiter-separated-case.js
+++ src/to-delimiter-separated-case.js
@@ -4,7 +4,6 @@ var isDefinedAndNotNull = kapheinJsTypeTrait.isDefinedAndNotNull;
 module.exports = (function ()
 {
     var _isAlphabetic = /[^.,/#!$%^&*;:{}=\-_`~()0-9]/;
-    var _isPunctuation = /[.,/#!$%^&*;:{}=\-_`~()]/;
 
     /**
      *  @param {string} text
@@ -72,47 +71,7 @@ module.exports = (function ()
         return tokens.join(delimiter);
     }
 
-    /**
-     *  @param {string} text
-     *  @param {string | RegExp} [delimiter]
-     *  @param {{
-            capitalizeInitial? : boolean;
-        }} [option]
-     */
-    function toCapitalizedCase(text)
-    {
-        var delimiter = arguments[1];
-        if(!isDefinedAndNotNull(delimiter))
-        {
-            delimiter = _isPunctuation;
-        }
-
-        var option = arguments[2];
-        if(!isDefinedAndNotNull(option))
-        {
-            option = {};
-        }
-
-        var tokens = text.split(delimiter);
-        for(var i = 0; i < tokens.length; ++i)
-        {
-            var finalToken = tokens[i].toLowerCase();
-            if(finalToken.length > 0)
-            {
-                if(i > 0 || option.capitalizeInitial)
-                {
-                    finalToken = finalToken[0].toUpperCase() + finalToken.substring(1);
-                }
-            }
-
-            tokens[i] = finalToken;
-        }
-
-        return tokens.join("");
-    }
-
     return {
-        toDelimiterSeparatedCase : toDelimiterSeparatedCase,
-        toCapitalizedCase : toCapitalizedCase
+        toDelimiterSeparatedCase : toDelimiterSeparatedCase
     };
 })();
